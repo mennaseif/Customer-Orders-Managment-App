eec658c8a4207a0c4b3cbc4ff3c24724037572b0
Menna Seif <menna.seif.122@gmail.com>
2024-07-29 21:09:01 +0300

    new

--- api/middleware/checkEmailExist.js
+++ api/middleware/checkEmailExist.js
@@ -7,7 +7,7 @@ export const checkEmailExist = (req, res, next) => {
 
       connection.execute(`select email from customers where email ='${req.body.email}'`, (err,data) => {
 
-         if(data.length != 0) return res.status(409).json({message:"email is already exist please try another email."})
+         if(data?.length != 0) return res.status(409).json({message:"email is already exist please try another email."})
             req.body.password = bycryptjs.hashSync(req.body.password,8)
 
          next()
